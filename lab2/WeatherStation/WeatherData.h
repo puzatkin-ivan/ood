68ae64e680c5cc2a7d99fba98d89681498fb85e5
puzatkin-ivan <puzatkin1998@gmail.com>
2018-10-04 00:48:17 +0300

    Добавил программу по паттерну Наблюдатель

---
+++ lab2/WeatherStation/WeatherData.h
@@ -0,0 +1,86 @@
+﻿#pragma once
+#include <memory>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include "Observer.h"
+#include "StreamBehaviors.h"
+
+using namespace std;
+
+class CDisplay: public IObserver<SWeatherInfo>
+{
+private:
+	void Update(SWeatherInfo const& data) override
+	{
+		std::cout << "Current Temp " << data.temperature << std::endl;
+		std::cout << "Current Hum " << data.humidity << std::endl;
+		std::cout << "Current Pressure " << data.pressure << std::endl;
+		std::cout << "----------------" << std::endl;
+	}
+};
+
+class CStatsDisplay : public IObserver<SWeatherInfo>
+{
+public:
+	CStatsDisplay(std::unique_ptr<IStreamBehaviors>&& streamBehaviors)
+		:m_streamBehaviors(std::move(streamBehaviors))
+	{
+	}
+
+private:
+	void Update(SWeatherInfo const& data) override
+	{
+		m_streamBehaviors->Stream(data);
+	}
+
+	std::unique_ptr<IStreamBehaviors> m_streamBehaviors;
+};
+
+class CWeatherData : public CObservable<SWeatherInfo>
+{
+public:
+	// Температура в градусах Цельсия
+	double GetTemperature()const
+	{
+		return m_temperature;
+	}
+	// Относительная влажность (0...100)
+	double GetHumidity()const
+	{
+		return m_humidity;
+	}
+	// Атмосферное давление (в мм.рт.ст)
+	double GetPressure()const
+	{
+		return m_pressure;
+	}
+
+	void MeasurementsChanged()
+	{
+		NotifyObservers();
+	}
+
+	void SetMeasurements(double temp, double humidity, double pressure)
+	{
+			m_humidity = humidity;
+		m_temperature = temp;
+		m_pressure = pressure;
+
+		MeasurementsChanged();
+	}
+protected:
+	SWeatherInfo GetChangedData() const override
+	{
+		SWeatherInfo info;
+		info.temperature = GetTemperature();
+		info.humidity = GetHumidity();
+		info.pressure = GetPressure();
+		return info;
+	}
+private:
+	double m_temperature = 0.0;
+	double m_humidity = 0.0;	
+	double m_pressure = 760.0;	
+};
