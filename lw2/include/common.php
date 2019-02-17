29667dc81c9f9edb42fc910386ca7cff0a8d2829
puzatkin-ivan <puzatkin1998@gmail.com>
2019-02-18 00:53:28 +0300

    Добавил забытые файлы

---
+++ lw2/include/common.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . '/../src/calculator/StatsCalculator.php';
+require_once __DIR__ . '/../src/observer/ObserverInterface.php';
+require_once __DIR__ . '/../src/observer/StatsDisplay.php';
+require_once __DIR__ . '/../src/observer/Display.php';
+require_once __DIR__ . '/../src/observable/ObservableInterface.php';
+require_once __DIR__ . '/../src/observable/Observable.php';
+require_once __DIR__ . '/../src/observable/WeatherData.php';
+require_once __DIR__ . '/../src/WeatherStation.php';
---
+++ lw2/src/calculator/StatsCalculator.php
@@ -0,0 +1,37 @@
+<?php
+
+class StatsCalculator
+{
+    /** @var string */
+    private $type;
+    /** @var float */
+    private $max = PHP_FLOAT_MIN;
+    /** @var float */
+    private $min = PHP_FLOAT_MAX;
+    /** @var int */
+    private $acc;
+    /** @var int */
+    private $count = 0;
+
+    public function __construct(string $type)
+    {
+        $this->type = $type;
+    }
+
+    public function calculate(float $data)
+    {
+        $this->max = max([$this->max, $data]);
+        $this->min = min([$this->min, $data]);
+        $this->acc += $data;
+        ++$this->count;
+        }
+
+    public function printData()
+    {
+        $result = 'Max ' . $this->type . ' ' . round($this->max, 3) . PHP_EOL;
+        $result .= 'Min ' . $this->type . ' ' . round($this->min, 3) . PHP_EOL;
+        $result .= 'Average ' . $this->type . ' ' . round($this->acc / $this->count, 3) . PHP_EOL;
+        $result .= '----------------';
+        return $result;
+    }
+}
